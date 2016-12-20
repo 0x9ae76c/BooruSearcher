acdf0a4c4aaccfe24b0865116fb7b83ed9a29dd2
Tensei <davuttensei@gmail.com>
2016-12-20 03:37:34 +0100

    SWEATSINY

---
+++ BooruSearcher/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using MetroRadiance.Chrome;
+using MetroRadiance.Interop.Win32;
+using Microsoft.Win32;
+
+namespace BooruSearcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+		public MainWindow() {
+			this.InitializeComponent();
+		}
+
+		private void OpenFileDialog(object sender, RoutedEventArgs e) {
+			var dialog = new OpenFileDialog();
+			if (dialog.ShowDialog() ?? false) {
+
+			}
+		}
+
+		private async void Hide(object sender, RoutedEventArgs e) {
+			this.Hide();
+
+			await Task.Delay(2500);
+
+			this.Visibility = Visibility.Visible;
+		}
+
+		protected override void OnContentRendered(EventArgs e) {
+			base.OnContentRendered(e);
+			//SetGlowingForActiveWindow();
+		}
+
+		//private static async void SetGlowingForActiveWindow() {
+		//	await Task.Delay(2500);
+
+		//	var hWnd = User32.GetForegroundWindow();
+		//	var appHandles = Application.Current.Windows
+		//		.OfType<Window>()
+		//		.Select(x => PresentationSource.FromVisual(x) as HwndSource)
+		//		.Where(x => x != null)
+		//		.Select(x => x.Handle)
+		//		.ToArray();
+		//	if (appHandles.All(x => x != hWnd)) {
+		//		var external = new ExternalWindow(hWnd);
+		//		var chrome = new WindowChrome();
+		//		chrome.Attach(external);
+		//	}
+		//}
+	}
+}
