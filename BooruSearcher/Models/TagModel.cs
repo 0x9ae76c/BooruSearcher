5a85619d2eb7b1fbf8213ec9e87f984d2248b88a
Tensei <davuttensei@gmail.com>
2016-12-20 05:00:37 +0100

    hmmmm

---
+++ BooruSearcher/Models/TagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BooruSearcher.ViewModels;
+using PropertyChanged;
+
+namespace BooruSearcher.Models {
+	[ImplementPropertyChanged]
+	public class TagModel {
+		public string Name { get; set; }
+
+		public ICommand RemoveCommand { get; }
+		public TagModel() {
+			RemoveCommand = new ActionCommand(Remove);
+		}
+
+		private void Remove() {
+			MainViewViewModel.Instance._tags.Remove(this);
+		}
+	}
+}
---
+++ BooruSearcher/ViewModels/MainViewViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooruSearcher.Models;
+using PropertyChanged;
+
+namespace BooruSearcher.ViewModels {
+	[ImplementPropertyChanged]
+	public class MainViewViewModel {
+
+		public ObservableCollection<TagModel> _tags = new ObservableCollection<TagModel>();
+		public ObservableCollection<ImageModel> _images = new ObservableCollection<ImageModel>();
+		public  ReadOnlyObservableCollection<TagModel> Tags { get; }
+
+		public ReadOnlyObservableCollection<ImageModel> Images { get; }
+
+		public static MainViewViewModel Instance;
+
+		public MainViewViewModel() {
+			Instance = this;
+			Tags = new ReadOnlyObservableCollection<TagModel>(_tags);
+			Images = new ReadOnlyObservableCollection<ImageModel>(_images);
+
+
+			_images.Add( new ImageModel {
+				Url = new Uri("https://avatars1.githubusercontent.com/u/6447030?v=3&s=460"),
+				Name = "Tensei"
+			});
+
+			_tags.Add(new TagModel {
+				Name = "hxyivyi,"
+			});
+		}
+	}
+}
