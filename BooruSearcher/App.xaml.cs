554bf6591c92cf3532e4fa4e0583494c81ef69f7
Tensei <davuttensei@gmail.com>
2016-12-20 04:03:48 +0100

    rename stuff and viewmodel for mainwindow

--- BooruSearcher/App.xaml.cs
+++ BooruSearcher/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using System.Windows;
+using BooruSearcher.ViewModels;
 
 namespace BooruSearcher
 {
@@ -13,5 +16,11 @@ namespace BooruSearcher
     /// </summary>
     public partial class App : Application
     {
-    }
+		private void AppStartup(object sender, StartupEventArgs args) {
+			var mainWindow = new MainWindow {
+				DataContext = new MainWindowViewModel()
+			};
+			mainWindow.Show();
+		}
+	}
 }
